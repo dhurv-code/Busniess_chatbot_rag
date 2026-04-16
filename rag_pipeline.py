61f8c2ad57e0ef6c52911264808f873d26398dba
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-04-16 15:52:23 +0530

    updated pipeline  commit

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -18,7 +18,7 @@ retriever = db.as_retriever(search_kwargs={"k": 3})
 
 pipe = pipeline(
     "text2text-generation",
-    model="google/flan-t5-large",
+    model="google/flan-t5-base",
     max_new_tokens=200,
     temperature=0.5
 )
